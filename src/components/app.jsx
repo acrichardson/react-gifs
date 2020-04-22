228abe8f0f58a94328ebb9f23a8355ade953f70f
Ashanti Richardson <ashanti.richardson@gmail.com>
2020-04-22 14:46:55 +0200

    making reactive

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 class App extends Component {
   constructor(props) {
@@ -10,28 +11,44 @@ class App extends Component {
     this.state = {
       gifs: [],
       selectedGifId: "7ltN7lCgF2MQE"
-    }
+    };
+  }
+
+  search = (query) => {
+    // API Call
+    giphy('tvUADK9NylWO9Xx8v8T9QBUyo8NgaRni').search({
+        q: query,
+        rating: 'g',
+        limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
   }
 
   render() {
-
-
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
             <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
       );
   }
 }
-
 
 export default App;
 
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   handleUpdate = (event) => {
-    console.log(event.target.value);
+    this.props.searchFunction(event.target.value);
   }
 
   render() {
     return (
       <input type="text" className="form-control form-search"
-        onChange={this.handleUpdate}/>
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
